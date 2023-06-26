73c7375cc13abde421afa61110dc8dcad0dd1860
FlavioPonciano <flavioponciano.dev@gmail.com>
2023-06-26 20:38:35 -0300

    Upando arquivos
    Iniciando a parte de escopos das funções

---
+++ escopo-de-variaveis/index.js
@@ -0,0 +1,5 @@
+/*O JS possuí 3 tipos de escopos:
+Global é o escopo do script como um todo.
+Função corresponde ao conteúdo inserido em um bloco de função {}.
+Bloco são as instruções incluídas dentro de comandos como if e switch.
+*/

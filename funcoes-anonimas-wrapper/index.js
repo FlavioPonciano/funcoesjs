c59a892bb4e0f5794e041a1ac1c01132e5846484
FlavioPonciano <flavioponciano.dev@gmail.com>
2023-06-28 20:44:52 -0300

    Upando arquivos
    Funções Anônimas

---
+++ funcoes-anonimas-wrapper/index.js
@@ -0,0 +1,8 @@
+//Funções anônimas são funções que não tem nome
+//Criando uma função anônima:
+//Podemos envelopar uma função dentro de uma variável
+var exibirSaudacao = function(nome) {
+    document.write('Olá, ' + nome + ', tudo bem?')
+}
+
+exibirSaudacao('Flávio')
